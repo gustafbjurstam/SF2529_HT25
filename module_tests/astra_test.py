8aac313eeb61fc5ad8b6dfa7f8b0e77168e66b90
lepamacka <88081225+lepamacka@users.noreply.github.com>
2025-08-22 17:19:10 +0200

    Update astra_test.py

--- module_tests/astra_test.py
+++ module_tests/astra_test.py
@@ -1,3 +1,6 @@
+# This script is for testing if ASTRA Toolbox can make use of a local GPU, which is necessary to run GPU-accelerated ODL code. 
+# For labs 1 and 2, GPU acceleration is not necessary. For labs 3 and 4, the plan is to provide access to a server with an appropriate GPU.
+
 import astra
 
 astra.test()
