c59007256f6d06d01466e2d79da3c4023839ae3a
lepamacka <88081225+lepamacka@users.noreply.github.com>
2025-08-22 17:27:04 +0200

    Update odl_test.py

--- module_tests/odl_test.py
+++ module_tests/odl_test.py
@@ -1,3 +1,6 @@
+# This script can be used to troubleshoot problems with ODL or the environment. Students should not run these tests needlessly.
+# Note that the current version (0.8.2) of ODL skips many tests and throws a lot of errors, even when working as intended.
+
 import odl
 
 odl.test()
